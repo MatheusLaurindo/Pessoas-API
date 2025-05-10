958bcb1af860646b5fbd0720274dfa682b1f2f5b
Matheus Figueredo <matheuslaurindo2017@gmail.com>
2025-05-10 14:19:22 -0300

    melhoria da documentacao swagger

--- Pessoas.API/Program.cs
+++ Pessoas.API/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Pessoas.API.Infra;
 using Pessoas.API.Repositories;
 using Pessoas.API.Repositories.Interfaces;
@@ -29,6 +30,14 @@ builder.Services.AddScoped<IHttpContextAcessorService, HttpContextAcessorService
 
 builder.Services.AddSwaggerGen(c =>
 {
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Pessoas.API",
+        Description = "API para gerenciamento de pessoas e autenticação JWT.",
+    });
+
+    // Habilita comentários XML para documentação
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
@@ -90,8 +99,8 @@ app.UseSwagger();
 
 app.UseSwaggerUI(options =>
 {
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Pessoas.API v1");
+    options.DocumentTitle = "Pessoas.API Docs";
 });
 
 using (var scope = app.Services.CreateScope())
